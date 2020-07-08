f8c817177869e6684184bacb7339218a92477eb9
Eunice Sanvee <44098832+esanvee@users.noreply.github.com>
2020-07-08 09:40:28 -0400

    Ready for review

--- src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
@@ -1,6 +1,51 @@
 package hbcu.stay.ready.mastering_loops;
 
+public class TriangleUtilities {package hbcu.stay.ready.mastering_loops;
 public class TriangleUtilities {
+    public static String getTriangle(int numberOfRows) {
+        String r = "";
+        for(int i = 0; i < numberOfRows; i++)
+        {
+            for(int j = 0; j < numberOfRows; j++)
+            {
+                r = r + "*";
+            }
+            System.out.println();
+        }
+        return r;
+    }
+    public static String getRow(int numberOfStars) {
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.print("*");
+        }
+    }
+    public static String getSmallTriangle() {
+        //
+        int i, j;
+        for(i = 0; i<4; i++)
+        {
+            for (j=0; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return "*";
+    }
+    public static String getLargeTriangle() {
+        int i, j;
+        for(i = 0; i<10; i++)
+        {
+            for (j=0; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return "*";
+    }
+}
 
     public static String getTriangle(int numberOfRows) {
         return null;
